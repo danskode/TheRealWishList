92d9cb8a5b326d841d5d8e87932a002c913b8952
danskode <149320369+danskode@users.noreply.github.com>
2024-10-29 12:33:34 +0100

    Initial commit

---
+++ src/main/java/org/kea/therealwishlist/TheRealWishListApplication.java
@@ -0,0 +1,13 @@
+package org.kea.therealwishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealWishListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRealWishListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kea/therealwishlist/TheRealWishListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kea.therealwishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRealWishListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
