f08d5344d03fd998fc2c539c0b6f11f8edaecade
danskode <149320369+danskode@users.noreply.github.com>
2024-11-02 12:10:28 +0100

    Ændret hvordan vi sætter databaseforbindelsen op i ProfileRepository, så vi fortsat kan arbejde med DriverManager i kombi med profiler for test og prod. Bør også ændres i WishRepository eller omvendt, så de bruger (som bruger DataSource, nemmere men mere high level)

--- src/main/java/org/kea/therealwishlist/repository/ProfileRepository.java
+++ src/main/java/org/kea/therealwishlist/repository/ProfileRepository.java
@@ -1,6 +1,7 @@
 package org.kea.therealwishlist.repository;
 
 import org.kea.therealwishlist.model.Profile;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
@@ -8,9 +9,20 @@ import java.sql.*;
 @Repository
 public class ProfileRepository {
 
-    final private String url = System.getenv("DB_URL");
-    final private String user = System.getenv("DB_USER");
-    final private String password = System.getenv("DB_PASSWORD");
+    // Her hentes variable fra den aktive profil angivet i application.properties (prod eller test) ...
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    //Denne kode gør, at man ikke kan bruge de forskellige profiler prod og test til forbindelsen ...
+    //final private String url = System.getenv("DB_URL");
+    //final private String user = System.getenv("DB_USER");
+    //final private String password = System.getenv("DB_PASSWORD");
 
 
     // Metode til at oprette en ny profil
